b534d0e20d37b2222108033b50f9e38d08efb7dc
sorinconstantin341C1 <sonixjams@gmail.com>
2018-03-29 19:54:35 +0300

    3rd commit

--- TestApp/app/src/main/java/ro/pub/cs/systems/eim/testapp/TestActivity.java
+++ TestApp/app/src/main/java/ro/pub/cs/systems/eim/testapp/TestActivity.java
@@ -4,13 +4,64 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class TestActivity extends AppCompatActivity {
+
+    private EditText eText = null;
+    private Button nButton = null;
+    private Button sButton = null;
+    private Button wButton = null;
+    private Button eButton = null;
+
+    private ButtonClickListener buttonClickListener = new ButtonClickListener();
+    private class ButtonClickListener implements View.OnClickListener {
+        @Override
+        public void onClick(View view) {
+            switch(view.getId()) {
+                case R.id.n_button:
+                    int nNumberOfClicks = Integer.parseInt(eText.getText().toString());
+                    nNumberOfClicks++;
+                    eText.setText(String.valueOf(nNumberOfClicks));
+                    break;
+                case R.id.s_button:
+                    int sNumberOfClicks = Integer.parseInt(eText.getText().toString());
+                    sNumberOfClicks++;
+                    eText.setText(String.valueOf(sNumberOfClicks));
+                    break;
+                case R.id.e_button:
+                    int eNumberOfClicks = Integer.parseInt(eText.getText().toString());
+                    eNumberOfClicks++;
+                    eText.setText(String.valueOf(eNumberOfClicks));
+                    break;
+                case R.id.w_button:
+                    int wNumberOfClicks = Integer.parseInt(eText.getText().toString());
+                    wNumberOfClicks++;
+                    eText.setText(String.valueOf(wNumberOfClicks));
+                    break;
+            }
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_test);
+
+        eText = (EditText)findViewById(R.id.an_edit_text);
+        eText.setText(String.valueOf(0));
+
+        nButton = (Button)findViewById(R.id.n_button);
+        sButton = (Button)findViewById(R.id.s_button);
+        eButton = (Button)findViewById(R.id.e_button);
+        wButton = (Button)findViewById(R.id.w_button);
+
+        nButton.setOnClickListener(buttonClickListener);
+        sButton.setOnClickListener(buttonClickListener);
+        eButton.setOnClickListener(buttonClickListener);
+        wButton.setOnClickListener(buttonClickListener);
     }
 
     @Override
